c37e542fc40d22932f6724c513c47fb8a3ed6573
prv0011 <kmaji@hertz.com>
2015-12-19 18:42:08 -0500

    test added

---
+++ src/test/java/com/crap/CrapTest.java
@@ -0,0 +1,13 @@
+package com.crap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+public class CrapTest {
+
+    @Test
+    public void contextLoads() {
+        assertEquals("Test", "Test");
+    }
+}
